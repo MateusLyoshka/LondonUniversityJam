5e25bd4595fd013a7878eeedf4724a36a79823bb
jc4725 <jc4725@ic.ac.uk>
2026-03-14 12:20:18 +0000

    Added basic skeleton for enemy movements

---
+++ enemy.gd
@@ -0,0 +1,42 @@
+extends Node2D
+
+@export var ENEMY_BASE_MOVE_SPEED = 10
+@export var target: Sprite2D
+
+var default_sprite: Texture2D = load("res://icon.svg")
+
+class EnemyStats:
+	var health: int
+	var speed: float
+	var texture: Texture2D
+	
+	func _init(h: int, s: float, t: Texture) -> void:
+		health = h
+		speed = s
+		texture = t
+
+var health_stats = {
+	1: EnemyStats.new(1, 30, default_sprite),
+	2: EnemyStats.new(2, 50, default_sprite)
+}
+var health = 2
+var currentStat: EnemyStats = EnemyStats.new(1, 50, default_sprite)
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	currentStat = health_stats[health]
+	updateSprite()
+
+func _physics_process(delta: float) -> void:
+	if target == null:		
+		return
+		
+	var direction = (target.global_position - global_position).normalized()
+	global_position += direction * ENEMY_BASE_MOVE_SPEED * delta
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+func updateSprite() -> void:
+	$Sprite2D.texture = currentStat.texture
